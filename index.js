9ec6444243ed0b703db86398e0c516d5498a0bc8
oldgeese <hiro@nestofgeese.local>
2017-01-29 09:15:05 +0900

    fixed to use process.env.PORT

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var http = require('http');
 var httpProxy = require('http-proxy');
+var port = process.env.PORT || 8080;
 
-httpProxy.createProxyServer({target: 'http://charasheet.vampire-blood.net/'}).listen(8080);
+httpProxy.createProxyServer({target: 'http://charasheet.vampire-blood.net/'}).listen(port);
