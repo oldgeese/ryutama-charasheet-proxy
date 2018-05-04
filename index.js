fd42d92b42c8cbcda4b5ad772ffed819313b9675
oldgeese <hiro.onoda@gmail.com>
2018-05-05 00:01:48 +0900

    change http to https

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ var http = require('http');
 var httpProxy = require('http-proxy');
 var port = process.env.PORT || 8080;
 
-httpProxy.createProxyServer({target: 'http://charasheet.vampire-blood.net/'}).listen(port);
+httpProxy.createProxyServer({target: 'https://charasheet.vampire-blood.net/'}).listen(port);
